f4674d47bba894ac5101cfb24fd016c3f5ec7a5c
Daniel Mauricio Naranjo Benavides <naranjobenavidesd@gmail.com>
2022-05-02 19:36:44 -0500

    Se agrega la opción de editar y se finaliza el crud

--- src/App.js
+++ src/App.js
@@ -1,47 +1,83 @@
-import React, {useState} from 'react';
-import UserTable from './components/UserTable';
-import {v4 as uuidv4} from 'uuid';
-import AddUserForm from './components/AddUserForm';
+import React, { useState } from "react";
+import UserTable from "./components/UserTable";
+import { v4 as uuidv4 } from "uuid";
+import EditUserForm from "./components/EditUserForm";
+import AddUserForm from "./components/AddUserForm";
 
 const App = () => {
   const usersData = [
-    { id: uuidv4(), name: 'Pepito', username: 'Night' },
-    { id: uuidv4(), name: 'Perez', username: 'Barriga' },
-    { id: uuidv4(), name: 'Berlin', username: 'El capo' },
-  ]
+    { id: uuidv4(), name: "Pepito", username: "Night" },
+    { id: uuidv4(), name: "Perez", username: "Barriga" },
+    { id: uuidv4(), name: "Berlin", username: "El capo" },
+  ];
 
   // State
   const [users, setUsers] = useState(usersData);
 
   //Agregar usuarios
   const addUser = (user) => {
-    user.id = uuidv4()
-    setUsers([
-      ...users,
-      user
-    ])
-  }
+    user.id = uuidv4();
+    setUsers([...users, user]);
+  };
 
   //Eliminar usuarios
   const deleteUser = (id) => {
-    setUsers(users.filter(user => user.id !== id))
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  //Editar usuario
+  const [editing, setEditing] = useState(false);
+  
+  const [currentUser, setCurrentUser] = useState({
+    id: null, name: '', username: ''
+  })
+
+  const editRow = (user ) => {
+    setEditing(true);
+    setCurrentUser({
+      id: user.id, name: user.name, username: user.username
+    })
   }
+
+  const updateUser = (id, updateUser) => {
+    setEditing(false);
+
+    setUsers(users.map(user => (user.id === id ? updateUser: user)))
+  }
+
+
 
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Agregar usuario</h2>
-          <AddUserForm addUser = {addUser}/>
+          {
+            editing ? (
+                <div>
+                  <h2>Editar Usuario</h2>
+                  <EditUserForm 
+                  currentUser = {currentUser}
+                  updateUser = {updateUser}
+                  />
+                </div>
+            ) : (
+                <div>
+                  <h2>Agregar usuario</h2>
+                  <AddUserForm addUser = {addUser} />
+                </div>
+            )
+          }
         </div>
         <div className="flex-large">
           <h2>Ver usuario</h2>
-          <UserTable users = {users} deleteUser = {deleteUser}/>
+          <UserTable users = {users} deleteUser = {deleteUser} 
+          editRow = {editRow}
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/EditUserForm.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const EditUserForm = (props) => {
+  const {register, handleSubmit, setValue, formState: { errors },
+      } = useForm({
+          defaultValues: props.currentUser
+      });
+
+      setValue('name', props.currentUser.name)
+      setValue('username', props.currentUser.username)
+
+
+  const onSubmit = (data, e) => {
+    data.id = props.currentUser.id
+    props.updateUser(props.currentUser.id, data);
+    //Limpiar campos
+    e.target.reset();
+  };
+
+  return (
+    <form onSubmit = {handleSubmit(onSubmit)}>
+      <label>Nombre</label>
+      <input type="text" name = "name" 
+        {...register("name", {required: { value: true}})}/>
+      <div>
+        {errors.name && <div>Este campo es requerido</div>}
+      </div>
+      <label>Nombre de usuario</label>
+      <input type = "text" name = "username" 
+          {...register("username", {required: { value: true}})}/>
+      {errors.username && <div>Este campo es requerido</div>}
+      <button>Editar usuario</button>
+    </form>
+  );
+}
+
+export default EditUserForm;
